b7f711a8930133093d6b30d4601b54be4b3de687
unknown <mianpascual@gmail.com>
2021-01-01 19:50:49 +0100

    New class - Comments.java

---
+++ Chapter_01-Java_Building_Blocks/Tip_01-Java_Class_Structure/Comments.java
@@ -0,0 +1,27 @@
+public class Comments {
+
+	// single-line comment
+	
+	
+	/* multiple line-comment syntax */
+	
+	/*
+		multiple line-comment syntax
+	*/
+	
+	/*
+	* 	multiple line comment
+	*/
+	 
+	 
+	/** javadoc syntax */
+	
+	
+	/**
+		javadoc syntax
+	*/
+	
+	/**
+	 * 	javadoc syntax
+	 */
+}
